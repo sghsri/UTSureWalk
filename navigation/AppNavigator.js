092629c520d28563be1b95bf6da3d269a716c5f4
Chimdindu Orakwue <denalexorakwue1@gmail.com>
2018-10-27 20:56:13 -0500

    firebase implementation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import DriverTabNavigator from './DriverTabNavigator';
+import HomeScreen from '../screens/HomeScreen';
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  Main: {screen : HomeScreen},
+  DriverQueue: DriverTabNavigator,
 });
