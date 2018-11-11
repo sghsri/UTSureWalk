1dcaab18ac1600fd61c7a7b8085ee0bc9baf8df3
Anish Bose <anishbose26@gmail.com>
2018-11-11 12:22:38 -0600

    Add rider form submission

--- screens/RiderScreen.js
+++ screens/RiderScreen.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button, TextInput, Alert} from 'react-native';
 
 
 
 export default class RiderScreen extends React.Component {
-
+  
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      name: "",
+      phone: "",
+      pickup: "",
+      dropoff: "",
+     };
+  }
+  
+  onRideRequest = () => {
+    if(this.state.name.length == 0 || !this.state.name.trim()) {
+      Alert.alert("Name cannot be empty");
+      return;
+    } else if(this.state.phone.length == 0 || !this.state.phone.trim()) {
+      Alert.alert("Phone number cannot be empty");
+      return;
+    } else if(this.state.pickup.length == 0 || !this.state.pickup.trim()) {
+      Alert.alert("Pickup location cannot be empty");
+      return;
+    } else if(this.state.pickup.length == 0 || !this.state.pickup.trim()) {
+      Alert.alert("Dropoff location cannot be empty");
+      return;
+    }
+
+    fetch('https://react-test-79a3b.firebaseio.com/rideRequests.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: this.state.name, 
+        phone: this.state.phone,
+        pickup: this.state.pickup,
+        dropoff: this.state.dropoff,
+      })
+    })
+
+    this.props.navigation.navigate("RiderSubmission");
+
   }
 
   render() {
@@ -16,22 +50,50 @@ export default class RiderScreen extends React.Component {
 
     return (
       <View style = {{paddingTop:20}}>
-        <Text>Rider Home!</Text>
-        <Button
-          onPress={() => fetch('https://react-test-79a3b.firebaseio.com/queue.json', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                      firstItem: 'first data point',
-                      secondItem: 'second data point'
-                    })
-                  })}
-          title="POST to firebase"
-          color="#841584"
-        />
-
         <Button title= "< Home" onPress={() =>
-            navigate('Main', {})
-            } />
+          navigate('Main', {})
+        } />
+        <Text style={{marginTop:20}}>Rider Home!</Text>
+        <TextInput style={{width:200, height: 40, borderWidth:1}}
+          value={this.state.name}
+          onChangeText={(text) => {this.setState({name: text})}}
+          placeholder="name"
+          autoCapitalize="none"
+          autoCorrect={false} />
+
+        <View style={{paddingTop:10}} />
+
+        <TextInput style={{width:200, height: 40, borderWidth:1}}
+          value={this.state.phone}
+          onChangeText={(text) => {this.setState({phone: text})}}
+          placeholder="phone"
+          keyboardType="phone-pad"
+          autoCapitalize="none"
+          autoCorrect={false} />
+
+        <View style={{paddingTop:10}} />
+
+        <TextInput style={{width:200, height: 40, borderWidth:1}}
+          value={this.state.pickup}
+          onChangeText={(text) => {this.setState({pickup: text})}}
+          placeholder="pickup location"
+          autoCapitalize="none"
+          autoCorrect={false} />
+
+        <View style={{paddingTop:10}} />
+
+        <TextInput style={{width:200, height: 40, borderWidth:1}}
+          value={this.state.dropoff}
+          onChangeText={(text) => {this.setState({dropoff: text})}}
+          placeholder="dropoff location"
+          autoCapitalize="none"
+          autoCorrect={false} />
+
+
+        <View style={{paddingTop:10}} />
+        <Button title="Submit Ride Request!" onPress={this.onRideRequest} />
+        <View style={{paddingTop:10}} />
+        
       </View>
     );
   }
